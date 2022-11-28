2b19f079fc1c1147e78fe8f5c4d36f433be185f3
Alan Castro <alan.ssuc2311@gmail.com>
2022-11-28 10:51:15 -0600

    Se agregan capturas de funcionalidad del programa

--- Interfaz/CodigoEnsamblador.Designer.cs
+++ Interfaz/CodigoEnsamblador.Designer.cs
@@ -95,7 +95,6 @@
             this.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.ClientSize = new System.Drawing.Size(818, 621);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "CodigoEnsamblador";
